58b61b51d58d2bb78a0b308f2d98c5e332f885fa
MarcP70 <marcpourias@yahoo.fr>
2024-06-03 11:56:55 +0200

    Added 8-payments.js for task 8

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,20 @@
+// Fonction processPayment
+function processPayment(amount) {
+    console.log(`Collecting payment of ${amount}`);
+}
+
+// Fonction processOrder
+function processOrder(orderId, amount) {
+    console.log(`${orderId} is being processed`);
+    processPayment(amount);
+    console.log(`${orderId} has been fully processed`);
+}
+
+// Partie principale du code
+console.log("Processing orders");
+
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+
+console.log("All the orders have been processed");
